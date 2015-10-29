7f2c3e633839b2399b82075480f1d300ba8622d9
Jay Luker <lbjay@reallywow.com>
2015-10-29 13:19:21 -0400

    * revert back to .cfg file (using env vars for aws creds is problematic) * need to use https connection to loggly (duh)

--- S3CloudfrontToLoggly.py
+++ S3CloudfrontToLoggly.py
@@ -1,5 +1,6 @@
 # AWS Lambda python handler to push Cloudfront logs from s3 to loggly
 
+import os
 import csv
 import boto
 import gzip
@@ -8,9 +9,13 @@ import logging
 from StringIO import StringIO
 from tempfile import NamedTemporaryFile
 from ConfigParser import ConfigParser
-from httplib import HTTPConnection, HTTPException
+from httplib import HTTPSConnection, HTTPException
 
 log = logging.getLogger()
+log_format = '%(asctime)-15s %(levelname)s %(name)s %(module)s:%(funcName)s:%(lineno)s - %(message)s'
+log_format=logging.Formatter(log_format)
+for h in log.handlers:
+    h.setFormatter(log_format)
 log.setLevel(logging.INFO)
 
 log.info("Loading function")
@@ -21,8 +26,15 @@ config.read('config.cfg')
 if config.getboolean('config', 'debug'):
     log.setLevel(logging.DEBUG)
 
-s3 = boto.connect_s3()
-conn = HTTPConnection("logs-01.loggly.com")
+aws_access_key_id = config.get('config', 'aws_access_key_id')
+aws_secret_access_key = config.get('config', 'aws_secret_access_key')
+
+if aws_access_key_id:
+    log.debug("Using credentials-based s3 connection")
+    s3 = boto.connect_s3(aws_access_key_id, aws_secret_access_key)
+else:
+    log.debug("Using role-based s3 connection")
+    s3 = boto.connect_s3()
 
 def upload(fh, bucket):
 
@@ -31,32 +43,40 @@ def upload(fh, bucket):
     if config.getboolean('config', 'include_bucket_tag'):
         tags += ',%s' % bucket
     upload_path = '/bulk/%s/tag/%s' % (token, tags)
+
+    log.debug("uploading bulk events from %s", fh.name)
     log.debug("using upload_path: %s", upload_path)
 
+    fh.flush()
+    fh.seek(0)
+    file_body = fh.read()
+
+    conn = HTTPSConnection("logs-01.loggly.com")
+
     try:
-        conn.request("POST", upload_path, fh, {'Content-type': 'application/json'})
+        conn.request("POST", upload_path, file_body, {'Content-type': 'application/json'})
+        resp = conn.getresponse()
+        log.debug("response status: %d", resp.status)
+        log.debug("response: %s", resp.read())
+        if resp.status != 200:
+            raise Exception("%s %s" % resp.status, resp.reason)
     finally:
         conn.close()
-
-    resp = conn.getresponse()
-    if resp.status != 200:
-        raise Exception("%s %s: %s" % resp.status, resp.reason, resp.read())
-    log.debug("response: %s", resp.read())
 
 def lambda_handler(event, context):
     logging.debug("Received event: " + json.dumps(event, indent=2))
 
     # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    logging.info("bucket: %s, key: %s", bucket, key)
+    logging.info("bucket: %s, key: %s", bucket_name, key)
 
     try:
-        bucket = s3.get_bucket(bucket)
+        bucket = s3.get_bucket(bucket_name)
         obj = bucket.get_key(key)
     except Exception as e:
         log.error(e)
-        log.error('Error getting object %s from bucket %s', key, bucket)
+        log.error('Error getting object %s from bucket %s', key, bucket_name)
         raise
 
     gzip_steam = gzip.GzipFile(mode='rb', fileobj=StringIO(obj.read()))
@@ -77,24 +97,45 @@ def lambda_handler(event, context):
         if count % max_lines == 0:
             log.debug("count: %d", count)
             if tmp_file is not None:
-                tmp_file.flush()
-                tmp_file.seek(0)
                 try:
-                    log.debug("uploading bulk events from %s", tmp_file.name)
-                    upload(tmp_file, bucket)
+                    upload(tmp_file, bucket_name)
                 except Exception, e:
-                    log.error("Error during upload of %s from %s", key, bucket)
-                    log.error(e)
+                    log.error("Error during upload: %s", str(e))
                     raise
-                tmp_file.close()
+                finally:
+                    tmp_file.close()
             tmp_file = NamedTemporaryFile(mode='rb+')
         row['timestamp'] = "%sT%s.000Z" % (row['date'], row['time'])
+        
+        # convert stringified int/float values  
+        row = dict((k, str2num(v)) for k,v in row.iteritems())
+
         tmp_file.write(json.dumps(row) + "\n")
         count += 1
 
-    log.info("Uploaded %d total events", count)
-
     if tmp_file is not None:
+        upload(tmp_file, bucket_name)
         tmp_file.close()
 
+    log.info("Uploaded %d total events", count)
+
+
+# helpers for converting the stringified int/float values in the parsed log data
+# into their indexable format. not the most effficient way to do this, but more
+# scrutable than other methods i've seen
+def is_int(obj):
+    try: i = int(obj)
+    except: return False
+    return True
+
+def is_float(obj):
+    try: f = float(obj)
+    except: return False
+    return True
+
+def str2num(obj):
+    if not isinstance(obj, basestring): return obj
+    if is_int(obj): return int(obj)
+    if is_float(obj): return float(obj)
+    return obj
 
